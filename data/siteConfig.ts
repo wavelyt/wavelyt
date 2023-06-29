f16cf06974f3cbe23006191295a60e00a7f9852c
wavelyt <hello@wavelyt.com>
2023-06-29 17:26:09 +0100

    Remove unnecessary data.

--- data/siteConfig.ts
+++ data/siteConfig.ts
@@ -1,16 +1,14 @@
 import type { SiteConfig } from "@/lib/types";
-const mail = "hello@wavelyt.com";
 const siteConfig: SiteConfig = {
   avatar: "/wavelyt-logo.svg",
   siteUrl: "https://nextjs-mdx-blog-theme.vercel.app",
   siteName: "Wavelyt",
   siteDescription: "Unlock Your Business's Full Potential with Cutting-Edge Tech Solutions",
   siteThumbnail: "/wavelyt-logo.svg",
-  contact: mail,
+  contact: 'mail@wavelyt.com',
   nav: [
     { label: "About", href: "/about" },
     { label: "Blog", href: "/blog" },
-    { label: "Contact", href: `mailto:${mail}` },
   ],
   social: {
     github: "https://github.com/wavelyt",
